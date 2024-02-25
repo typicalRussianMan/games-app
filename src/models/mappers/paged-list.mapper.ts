449826023fae336d4245cd78f2bbcf55aaf41051
AndreySkripachev <andrey-skripachev@saritasa.com>
2024-02-26 01:45:58 +0700

    Add paged list model

---
+++ src/models/mappers/paged-list.mapper.ts
@@ -0,0 +1,23 @@
+import { PagedListDto } from '../dtos/paged-list.dto';
+import { PagedList } from '../paged-list';
+
+import { IMapperFromDto } from './mapper';
+
+/** Paged list mapper. */
+export class PagedListMapper<T, TDto> implements IMapperFromDto<PagedListDto<TDto>, PagedList<T>> {
+
+  public constructor(
+    private readonly mapper: IMapperFromDto<TDto, T>,
+  ) {
+  }
+
+  /** @inheritdoc */
+  public fromDto(data: PagedListDto<TDto>): PagedList<T> {
+    return new PagedList({
+      items: data.items.map(this.mapper.fromDto),
+      limit: data.limit,
+      offset: data.offset,
+      total: data.totalCount,
+    });
+  }
+}
