3b7c1931f919f2a665fcace6ceda566f706cae44
AndreySkripachev <andrey-skripachev@saritasa.com>
2024-02-27 15:21:54 +0700

    Add map component

---
+++ src/components/CompanyMap/CompanyMap.tsx
@@ -0,0 +1,35 @@
+import { FC, memo } from 'react';
+
+import { CompanyLite } from '../../models/company-lite';
+
+import './style.css';
+
+type Props = {
+
+  /** Company. */
+  company: CompanyLite;
+};
+
+const origin = 'https://yandex.ru/map-widget/v1/?';
+
+const buildMapLink = ({ address: { lat, lng } }: CompanyLite): string =>
+  // eslint-disable-next-line prefer-template
+  origin +
+  'll=' + lng.toString() + '%2C' + lat.toString() +
+  '&mode=poi' +
+  '&poi%5Bpoint%5D=' + lng.toString() + '2%C' + lat.toString() +
+  '&z=16';
+
+const CompanyMapComponent: FC<Props> = ({ company }) =>  (
+  <div className='container'>
+    <iframe
+      src={buildMapLink(company)}
+      height="400"
+      className='map'
+    ></iframe>
+    <div className="marker"></div>
+  </div>
+);
+
+/** Company map. */
+export const CompanyMap = memo(CompanyMapComponent);
--- src/pages/main/pages/DashboardGamePage/DashboardGamePage.tsx
+++ src/pages/main/pages/DashboardGamePage/DashboardGamePage.tsx
@@ -1,6 +1,53 @@
-import { FC } from 'react';
+import { FC, useEffect, useState } from 'react';
+import { CircularProgress } from '@mui/material';
+
+import { PagedList } from '../../../../models/paged-list';
+import { Game } from '../../../../models/game';
+import { gameApi } from '../../../../api';
+import { CompanyMap } from '../../../../components/CompanyMap';
+
+type Coordinates = Pick<GeolocationCoordinates, 'latitude' | 'longitude'>;
+
+const DEFAULT_MAP_CENTER: Coordinates = {
+  latitude: 56.035665,
+  longitude: 92.875777,
+};
 
 /** Dashboard game page. */
-export const DashboardGamePage: FC = () => (
-  <div className="123">aazazaz</div>
-);
+export const DashboardGamePage: FC = () => {
+  const [gameList, setGameList] = useState<PagedList<Game> | null>(null);
+  const [center, setCenter] = useState<Coordinates>(DEFAULT_MAP_CENTER);
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(
+      pos => {
+        setCenter(pos.coords);
+      },
+    );
+  }, []);
+
+  useEffect(() => {
+    gameApi.getGames({
+      bounds: {
+        bottom: center.latitude - 1,
+        left: center.longitude - 1,
+        right: center.longitude + 1,
+        top: center.latitude + 1,
+      },
+    }).then(games => {
+      setGameList(games);
+    });
+  }, [center]);
+
+  if (gameList === null) {
+    return <CircularProgress />;
+  }
+
+  const maps = gameList.items.map(e => (<CompanyMap company={e.company} />));
+
+  return (
+    <div>
+      {...maps}
+    </div>
+  );
+};
