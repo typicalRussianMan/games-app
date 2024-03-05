dfab95688d148ea23159201adaf24b146ce947ad
Andrey Skripachev <61969887+typicalRussianMan@users.noreply.github.com>
2024-03-05 17:32:59 +0700

    Feature/add achievements logic (#7)
    * Add achievement model and endpoint implementation
    
    * Add solution with interval
    
    * Code improvements
    
    * Use solution with interceptor
    
    * Add styles for achievement snackbar
    
    ---------
    
    Co-authored-by: AndreySkripachev <andrey-skripachev@saritasa.com>

---
+++ src/components/AchievementsProvider/AchievementsProvider.tsx
@@ -0,0 +1,92 @@
+import { FC, MouseEvent, memo, useEffect, useState } from 'react';
+import { Outlet } from 'react-router-dom';
+import { IconButton, Snackbar, Typography } from '@mui/material';
+import CloseIcon from '@mui/icons-material/Close';
+import { InternalAxiosRequestConfig } from 'axios';
+
+import { authApi, http } from '../../api';
+import { Achievement } from '../../models/achievement';
+
+import './style.css';
+
+type Interceptor = (conf: InternalAxiosRequestConfig) => InternalAxiosRequestConfig;
+type VFn = () => void;
+type Conf = InternalAxiosRequestConfig;
+
+const sendAchievementRequestAfterRequest = (onSend: VFn): Interceptor => (conf: Conf): Conf => {
+  if (conf.url && !conf.url.includes('check-achievements')) {
+    onSend();
+  }
+  return conf;
+};
+
+const AchievementProviderComponent: FC = () => {
+
+  const [updates, setUpdates] = useState<Achievement[]>([{ description: 'Play 1 game', title: 'Player I', id: 0 }]);
+  const [open, setOpen] = useState(false);
+
+  const onRequest = (): void => {
+    setTimeout(async() => {
+      const achievements = await authApi.checkAchievementUpdates();
+      const total = [...updates, ...achievements];
+      setUpdates(total);
+      setOpen(total.length > 0);
+    }, 1000);
+  };
+
+  useEffect(() => {
+    const id = http.interceptors.request.use(sendAchievementRequestAfterRequest(onRequest));
+
+    return () => http.interceptors.request.eject(id);
+  }, []);
+
+  useEffect(() => {
+    if (!open && updates.length > 0) {
+      setOpen(true);
+    }
+  }, [open]);
+
+  const handleClose = (): void => {
+    setOpen(false);
+    setUpdates(updates.slice(1));
+  };
+
+  const handleAchievementClick = (_e: MouseEvent): void => {
+    return void 0;
+  };
+
+  const handleAchievementClose = (e: MouseEvent): void => {
+    e.stopPropagation();
+    handleClose();
+  };
+
+  return (
+    <>
+      <Outlet />
+      <Snackbar
+        open={open}
+        autoHideDuration={3000}
+        onClose={handleClose}
+      >
+        <div className='snackbar' tabIndex={1} onClick={handleAchievementClick}>
+          <div className="image-block">
+            <img
+              width={75}
+              src="https://static.vecteezy.com/system/resources/previews/011/665/596/non_2x/3d-trophy-cup-icon-isolated-in-white-background-3d-rendering-png.png"
+            />
+          </div>
+          <div className="achievement-info">
+            <Typography variant='h5' component='div'>{updates[0]?.title}</Typography>
+            <Typography variant='body1' component='div'>{updates[0]?.description}</Typography>
+          </div>
+          <IconButton onClick={handleAchievementClose} color='primary' aria-label='close popup'>
+            <CloseIcon />
+          </IconButton>
+        </div>
+      </Snackbar>
+    </>
+  );
+};
+
+/** Achievement provider. */
+export const AchievementProvider = memo(AchievementProviderComponent);
---
+++ src/models/dtos/achievement.dto.ts
@@ -0,0 +1,12 @@
+/** Achievement DTO. */
+export type AchievementDto = {
+
+  /** ID. */
+  readonly id: number;
+
+  /** Title. */
+  readonly title: string;
+
+  /** Description. */
+  readonly description: string;
+};
